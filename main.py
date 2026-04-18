eac8b5021fdd330f77fb8e2ee8ca51c0f41d444c
AnupamMax-10 <chatterjeeanupam291@gmail.com>
2026-04-18 15:08:06 +0530

    Driver Sleep Detection Project

---
+++ main.py
@@ -0,0 +1,56 @@
+import cv2
+import winsound
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Camera not working")
+    exit()
+
+closed_eyes = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+
+
+        if len(eyes) == 0:
+            closed_eyes += 1
+        else:
+            closed_eyes = 0
+
+        if closed_eyes > 15:
+            cv2.putText(frame, "SLEEP ALERT!", (50,50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
+
+            if closed_eyes == 16:
+                winsound.Beep(1000, 500)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+
+    cv2.imshow("Sleep Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
